aaf25e7b220274a346d559db5620289a4b1187da
AyushV10 <av2410new@gmail.com>
2024-03-16 18:51:04 +0530

    Hoop stars game pushed

---
+++ Assets/Scripts/Controllers/Enemy/EnemyMovementController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DG.Tweening;
+using Enums;
+using Signals;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Controllers.Enemy
+{
+    public class EnemyMovementController : MonoBehaviour
+    {
+        private Rigidbody _enemyRigidbody;
+        private GameStates _gameStates;
+
+        private void Awake()
+        {
+            Init();
+        }
+        
+        private void Init()
+        {
+            _enemyRigidbody = GetComponent<Rigidbody>();
+            _gameStates = GameStates.Stop;
+        }
+        
+        
+
+        private void OnEnable()
+        {
+            SubscribeEvents();
+        }
+
+        private void SubscribeEvents()
+        {
+            CoreGameSignals.Instance.onChangeGameState += OnChangeGameState;
+            CoreGameSignals.Instance.onReset += TransformReset;
+        }
+
+        private void UnSubscribeEvents()
+        {
+            CoreGameSignals.Instance.onChangeGameState -= OnChangeGameState;
+            CoreGameSignals.Instance.onReset -= TransformReset;
+        }
+
+        private void OnDisable()
+        {
+            UnSubscribeEvents();
+        }
+
+        
+        private void FixedUpdate()
+        {
+            if (_gameStates == GameStates.Playing)
+            {
+                EnemyMove();
+            }
+        }
+        
+        private void OnChangeGameState(GameStates state)
+        {
+            _gameStates = state;
+            if (_gameStates == GameStates.Playing)
+            {
+                _enemyRigidbody.isKinematic = false;
+            }
+        }
+
+        private void EnemyMove()
+        {
+            if (_enemyRigidbody.position.x > 0 && _enemyRigidbody.position.y < 10)
+            {
+                EnemyLeftMove();
+            }
+            else if (_enemyRigidbody.position.x < 0 && _enemyRigidbody.position.y < 10)
+            {
+                EnemyRightMove();
+            }
+        }
+
+        private void EnemyLeftMove()
+        {
+            _enemyRigidbody.velocity = new Vector3(-2.5f,12,0);
+        }
+
+        private void EnemyRightMove()
+        {
+            _enemyRigidbody.velocity = new Vector3(2.5f,12,0);
+        }
+        
+        private void TransformReset()
+        {
+            _enemyRigidbody.isKinematic = true;
+            transform.position = new Vector3(3, 6, 0);
+            transform.DORotate(new Vector3(0,0,0), 0.1f);
+        }
+    }
+}
